bdaeff9ab3e2797e4225037e0425ca8c7b688480
nguyenhung <nguyenhung2004200@gmial.com>
2025-04-13 11:21:27 +0700

    Step 4(EditorsPick section) : Complete EditorsPick section, preview in Anh Minh Chung

--- Week8/week8_using_ai/src/App.jsx
+++ Week8/week8_using_ai/src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import RecipeVideos from "./components/RecipeVideos"
 import SummerRecipes from "./components/SummerRecipes"
+import EditorsPick from './components/EditorsPick';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <SummerRecipes/>
         <RecipeVideos/>
+        <EditorsPick/>
       </main>
     </div>
   )
