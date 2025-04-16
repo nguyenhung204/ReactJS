784b1d9613532fce16046c2be103b99bdefa7b9c
nguyenhung <nguyenhung2004200@gmial.com>
2025-04-16 10:15:12 +0700

    implement logic of redux to components

--- Week9/redux/src/App.jsx
+++ Week9/redux/src/App.jsx
@@ -1,13 +1,47 @@
-import './App.css'
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import Counter from './features/counter/Counter';
+import TodoList from './features/todos/TodoList';
+import ThemeToggler from './features/theme/ThemeToggler';
+import ShoppingCart from './features/cart/ShoppingCart';
+import Auth from './features/auth/Auth';
+import './App.css';
+import { useSelector } from 'react-redux';
 
-function App() {
+// ThemeWrapper component to apply theme
+function ThemeWrapper({ children }) {
+  const themeMode = useSelector(state => state.theme.mode);
   
-
   return (
-    <>
-      
-    </>
-  )
+    <div className={`app-container ${themeMode}-theme`}>
+      {children}
+    </div>
+  );
 }
 
-export default App
+// Main App component
+function App() {
+  return (
+    <Provider store={store}>
+      <AppContent />
+    </Provider>
+  );
+}
+
+// AppContent separated to use Redux hooks inside
+function AppContent() {
+  return (
+    <ThemeWrapper>
+      <h1>Redux Examples</h1>
+      <div className="features-container">
+        <Counter />
+        <TodoList />
+        <ThemeToggler />
+        <ShoppingCart />
+        <Auth />
+      </div>
+    </ThemeWrapper>
+  );
+}
+
+export default App;
